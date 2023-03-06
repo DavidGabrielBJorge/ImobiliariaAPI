c97402fec7b900dcbea7d9107aa72facc835193a
DavidGabrielBJorge <david.jorge@estudante.iftm.edu.br>
2023-03-06 11:26:43 -0300

    add: adicionando cadastrar um imovel

--- src/controller/imoveisController.js
+++ src/controller/imoveisController.js
@@ -5,7 +5,18 @@ class ImovelController{
     static listarTodos = (req, res) =>{
         imoveis.find((err,imoveis)=>{
         res.status(200).json(imoveis);
-    })
+        })
+    }
+
+    static cadastrar = (req, res) => {
+        let imovel = new imoveis(req.body);
+        imovel.save((err)=>{
+            if(err){
+                res.status(500).send({message: `${err.message} - falha ao cadastrar imovel!`})
+            }else{
+                res.status(201).send(imovel.toJSON())
+            }
+        })
     }
 }
 
--- src/routes/imoveisRouter.js
+++ src/routes/imoveisRouter.js
@@ -4,5 +4,6 @@ import ImovelController from "../controller/imoveisController.js";
 const router = express.Router();
 
 router.get("/imoveis", ImovelController.listarTodos)
+      .post("/imoveis", ImovelController.cadastrar)
 
 export default router
